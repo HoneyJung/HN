08024291bc19d2ac25792b7e9b1e30b98e6e72fe
HoneyJung <37574790+HoneyJung@users.noreply.github.com>
2020-07-09 11:22:27 +0900

    new crawler

--- crawler/new_crawl.py
+++ crawler/new_crawl.py
@@ -15,12 +15,13 @@ def Read_json():
     print(json_data)
 
 
-def Parsing(contest_text): # 이름, 주최, 날짜, 접수 여부, D-day
-    print(contest_text)
-    contest_list = contest_text.split('\n')
+def Parsing(contest_list): # 이름, 주최, 날짜, 접수 여부, D-day
+    print("hi")
+    print(contest_list)
+    #contest_list = contest_text.split('\n')
     contest_dict = {}
-    for i in range(1,len(contest_list)-1,5):
-        j = (i // 5) + 1
+    for i in range(0,len(contest_list),5):
+        #j = (i // 5) + 1
         contest_dict[contest_list[i]] = dict()
         contest_dict[contest_list[i]]['contest_host'] = contest_list[i+1]
         contest_dict[contest_list[i]]['contest_apply_period'] = contest_list[i+2]
@@ -32,7 +33,7 @@ def Parsing(contest_text): # 이름, 주최, 날짜, 접수 여부, D-day
             contest_dict[contest_list[i]]['contest_D_day'] = contest_list[i+4].split()[0]
             contest_dict[contest_list[i]]['contest_views_count'] = contest_list[i+4].split()[1]
 
-    with open('contest2.json','w', encoding='UTF-8-sig') as make_file: #### write on json file
+    with open('contest_ing.json','w', encoding='UTF-8-sig') as make_file: #### write on json file
         json.dump(contest_dict, make_file, ensure_ascii = False, indent = "\t")
 
 def Crawl_To_ThinkU():
@@ -41,51 +42,37 @@ def Crawl_To_ThinkU():
     driver.get('https://thinkyou.co.kr/')    # 크롤링할 사이트 호출
 
     try:    # 정상 처리
+
+        ##### 접수중 Click
         element = WebDriverWait(driver, 3).until(
             EC.presence_of_element_located((By.XPATH, '//*[@id="wrap"]/div[3]/div/div[5]/div[1]/a[3]'))
         )    # 해당 태그 존재 여부를 확인하기까지 3초 기다림
         driver.find_element_by_xpath('//*[@id="wrap"]/div[3]/div/div[5]/div[1]/a[3]').click()
         driver.implicitly_wait(10)
-        contest_text = ""
+        #contest_text = ""
         temp_contest_text = ''
-        total_contest_text = ''
-        while(True):
+        total_contest_list = []
+        for _ in range(8):
             contest_list = driver.find_element_by_xpath('//*[@id="contestArea"]')
-            if temp_contest_text == contest_list.text: # if it crawl to end, break!
-                print("break")
-                #print(temp_contest_text)
-                break
-            temp_contest_text = contest_list.text 
-            total_contest_text = total_contest_text + '\n' + temp_contest_text
-            #print(contest_text)
-
+            # if temp_contest_text == contest_list.text: # if it crawl to end, break!
+            #     print("break")
+            #     #print(temp_contest_text)
+            #     break
+            temp_contest_text = contest_list.text
+            total_contest_list.extend(temp_contest_text.split('\n')[1:-1])
             element = WebDriverWait(driver, 5).until(
             EC.element_to_be_clickable((By.XPATH, '//*[@id="contestArea"]/div[2]/a[3]'))
             )    # 해당 태그 존재 여부를 확인하기까지 3초 기다림
             element = driver.find_element_by_xpath('//*[@id="contestArea"]/div[2]/a[3]')
             driver.execute_script("arguments[0].click();", element)
             driver.implicitly_wait(5)
-
-
-            print(temp_contest_text)
-            print("hi\n\n\n")
-
-        Parsing(contest_text)
+            #print(temp_contest_text)
+            #print("hi\n\n\n")
+        Parsing(total_contest_list)
     except TimeoutException:    # 예외 처리
         print('해당 페이지에 해당 정보가 존재하지 않습니다.')
     finally:
         driver.quit() # 종료driver.quit()
-    # try:    # 정상 처리
-    #     element = WebDriverWait(driver, 3).until(
-    #         EC.presence_of_element_located((By.XPATH, '//*[@id="contestArea"]/div[1]'))
-    #     )    # 해당 태그 존재 여부를 확인하기까지 3초 기다림
-    #     contest_list = driver.find_element_by_xpath('//*[@id="contestArea"]/div[1]')
-    #     contest_text = contest_list.text
-    #     Parsing(contest_text)
-    # except TimeoutException:    # 예외 처리
-    #     print('해당 페이지에 해당 정보가 존재하지 않습니다.')
-    # finally:
-    #     driver.quit() # 종료driver.quit()
 
 
 if __name__ == '__main__':
